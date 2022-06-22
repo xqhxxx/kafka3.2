75b1d30b6a30df8645ce7dc0d9b20f3fed6b644d
xxx <xxx@xxx.com>
2022-06-22 16:14:05 +0800

    0622 1608

--- src/main/java/com/xxx/comsumers/MyConsumer.java
+++ src/main/java/com/xxx/comsumers/MyConsumer.java
@@ -21,12 +21,11 @@ public class MyConsumer {
     public static void main(String[] args) {
         Properties prop = new Properties();
         prop.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.0.4:9092");
+        prop.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        prop.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         prop.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
         prop.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, 1000);
         prop.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 30000);
-
-        prop.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        prop.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         prop.put(ConsumerConfig.GROUP_ID_CONFIG, "group_test_1");
 
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(prop);
